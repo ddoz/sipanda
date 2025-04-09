18d433abb0384bbd085483a23e5dae32d8fe828b
ddoz <adam.japal@gmail.com>
2025-04-09 11:24:12 +0700

    refactor login API to improve error handling and remove password from response

--- src/app/api/login/[rand]/route.ts
+++ src/app/api/login/[rand]/route.ts
@@ -1,30 +1,41 @@
-import { NextResponse } from 'next/server';
-import prisma from '@/lib/prisma';
-import { compare, compareSync, hash } from 'bcrypt';
+import { NextResponse } from "next/server";
+import prisma from "@/lib/prisma";
+import { compare } from "bcrypt";
 
-export async function POST(req: Request, res: Response) {
+export async function POST(req: Request) {
   try {
     const body = await req.json();
     const { username, password } = body;
+
     const user = await prisma.user.findFirst({
       where: {
-        email: username
-      }
+        email: username,
+      },
     });
 
-    if(!user) {
-      return NextResponse.json({ status: 'Username atau Password Salah' }, { status: 401 });
+    if (!user) {
+      return NextResponse.json(
+        { status: "Username atau Password Salah" },
+        { status: 401 },
+      );
     }
 
-    if (user && (await compare(password, user.password))) {
-      console.log(user);
-          return NextResponse.json(user, { status: 200 });
-    }else {
-        return null;
+    const passwordMatch = await compare(password, user.password);
+    if (passwordMatch) {
+      // Optional: hapus password dari response biar lebih aman
+      const { password, ...safeUser } = user;
+      return NextResponse.json(safeUser, { status: 200 });
+    } else {
+      return NextResponse.json(
+        { status: "Username atau Password Salah" },
+        { status: 401 },
+      );
     }
   } catch (e: any) {
     console.error(e);
-    return NextResponse.json({ status: 'fail', error: e.message }, { status: 500 });
+    return NextResponse.json(
+      { status: "fail", error: e.message },
+      { status: 500 },
+    );
   }
 }
-
